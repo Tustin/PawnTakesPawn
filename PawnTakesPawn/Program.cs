b7ed510ab9b85e9062a95221b06c1d4ef3f046fb
Tustin <Tustin@users.noreply.github.com>
2020-08-15 18:37:52 -0500

    grep stream links from page

--- PawnTakesPawn/Program.cs
+++ PawnTakesPawn/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace PawnTakesPawn
 {
@@ -6,7 +10,33 @@ namespace PawnTakesPawn
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using var client = new WebClient
+            {
+                Encoding = Encoding.UTF8
+            };
+
+            var contents = client.DownloadString("https://pawntakespawn.com/tv");
+
+            var doc = new HtmlDocument();
+            doc.LoadHtml(contents);
+
+            foreach (var tape in doc.DocumentNode.SelectNodes("//a[contains(@class, 'tape-fade-in')]"))
+            {
+                var attribute = tape.GetAttributeValue("xlink:href", string.Empty);
+                if (attribute == default)
+                {
+                    continue;
+                }
+
+                var match = Regex.Match(attribute, @"\(&#39;(.*)&#39;\)");
+
+                if (match.Groups.Count != 2)
+                {
+                    continue;
+                }
+
+                var streamUrl = WebUtility.HtmlDecode(match.Groups[1].ToString());
+            }
         }
     }
 }
